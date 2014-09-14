b981f0962547ac131c7510c0d545d63855775db6
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-09-14 18:44:02 -0300

    added point where it was missing

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 
 
 setup(
-    name='SerializedRedis',
+    name='serialized-redis',
     version='0.1.0',
     description='A solution to have lists and dictionaries serialized using redis-py.',
     author='EvandroLG',
     author_email='evandrolgoncalves@gmail.com',
-    keywords=['Redis', 'redis-py', 'serialized datas']
+    keywords=['Redis', 'redis-py', 'serialized datas'],
     url='http://github.com/EvandroLG/serialized-redis',
     licence='MIT',
     tests_require='pytest>=2.5.0',
