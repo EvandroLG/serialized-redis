f330bb120dc4130057191d722258a9e99025ef81
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-09-14 13:53:33 -0300

    created get method and added validation to set method

--- serialized_redis.py
+++ serialized_redis.py
@@ -1,11 +1,17 @@
 from redis import Redis
 import pickle
 
-
-class SerializedRedis(StrictRedis):
+""
+class SerializedRedis(Redis):
 	def set(self, key, value):
-		super(SerializedRedis, self).set(key, pickle.dumps(value))
+		is_list_or_dict = type value == 'list' or type value == 'dict'
 
-	# def get(self, key):
-		# pass
-		# super()
+		if is_list_or_dict:
+			super(Redis, self).set(key, pickle.dumps(value))
+			return
+
+		super(Redis, self).set(key, value)
+
+	def get(self, key):
+		value = super(Redis, self).get(key)
+		return pickle.loads(value)
