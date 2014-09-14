d776cac3ac6e1c2b852372ff00b11808096ed8ad
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-09-14 14:00:31 -0300

    added requirements file

--- serialized_redis.py
+++ serialized_redis.py
@@ -1,10 +1,10 @@
 from redis import Redis
 import pickle
 
-""
+
 class SerializedRedis(Redis):
 	def set(self, key, value):
-		is_list_or_dict = type value == 'list' or type value == 'dict'
+		is_list_or_dict = type(value) == list or type(value) == dict
 
 		if is_list_or_dict:
 			super(Redis, self).set(key, pickle.dumps(value))
