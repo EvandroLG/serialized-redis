84c414f06edbcd9a27035c645cab900cb0b8f347
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2014-10-05 19:19:23 -0300

    added method to execute another methods

--- serialized_redis/__init__.py
+++ serialized_redis/__init__.py
@@ -11,14 +11,17 @@ import pickle
 
 
 class SerializedRedis(Redis):
-    def set(self, key, value):
+    def _execute_command(self, key, value, method):
         is_list_or_dict = isinstance(value, list) or isinstance(value, dict)
 
         if is_list_or_dict:
-            super(Redis, self).set(key, pickle.dumps(value))
+            method(key, pickle.dumps(value))
             return
 
-        super(Redis, self).set(key, value)
+        method(key, value)
+
+    def set(self, key, value):
+        self._execute_command(key, value, super(Redis, self).set)
 
     def get(self, key):
         value = super(Redis, self).get(key)
@@ -31,15 +34,10 @@ class SerializedRedis(Redis):
         return value
 
     def rpushx(self, key, value):
-        is_list_or_dict = isinstance(value, list) or isinstance(value, dict)
-
-        if is_list_or_dict:
-            super(Redis, self).rpushx(key, pickle.dumps(value))
-
-        super(Redis, self).rpushx(key, value)
+        self._execute_command(key, value, super(Redis, self).rpushx)
 
     # def rpush(self, key, *values):
     #     pass
 
     # def lrange(self, key, start, end):
-    #     import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
